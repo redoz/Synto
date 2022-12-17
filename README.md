c4a19fdf29cf55b78f061328efa2a5b2e2968c14
Patrik Husfloen <redoz@redoz.com>
2022-12-17 16:31:01 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Synto
+
+This is an experimental library designed to make it easy to construct Syntax Trees. It does this by using a source-generator that turns syntax-trees as parsed by the compiler and "quotes" them to expose the syntax tree itself in your code.
+
+See test/Synto.Test/Samples.cs for some simple examples of what currently works.
--- test/Synto.Test/Samples.cs
+++ test/Synto.Test/Samples.cs
@@ -2,25 +2,24 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Text;
-using Microsoft.CodeAnalysis.CSharp;
 using Xunit;
 
 namespace Synto.Test;
 
-public partial class UnitTest1
+public partial class Samples
 {
-    private partial class SF { };
+    private partial class Factory { };
 
     [Fact]
     public void Test0()
     {
-        [Template(typeof(SF), Options = TemplateOption.Single)]
+        [Template(typeof(Factory), Options = TemplateOption.Single)]
         static void Simple()
         {
             Console.WriteLine("Hello World");
         }
 
-        StatementSyntax node = SF.Simple();
+        StatementSyntax node = Factory.Simple();
 
         var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
 
@@ -30,13 +29,13 @@ public partial class UnitTest1
     [Fact]
     public void Test1()
     {
-        [Template(typeof(SF), Options = TemplateOption.Single)]
+        [Template(typeof(Factory), Options = TemplateOption.Single)]
         static void Hello(string message)
         {
             Console.WriteLine("Hello " + message);
         }
 
-        StatementSyntax node = SF.Hello("World");
+        StatementSyntax node = Factory.Hello("World");
 
         var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
 
@@ -46,20 +45,20 @@ public partial class UnitTest1
     [Fact]
     public void Test2()
     {
-        [Template(typeof(SF), Options = TemplateOption.Single)]
+        [Template(typeof(Factory), Options = TemplateOption.Single)]
         static void Inner()
         {
             new StringBuilder("Something").Append("Foo").ToString();
         }
 
 
-        [Template(typeof(SF), Options = TemplateOption.Single)]
+        [Template(typeof(Factory), Options = TemplateOption.Single)]
         static void Outer(Syntax<string> message)
         {
             Console.WriteLine("Hello " + message());
         }
 
-        StatementSyntax node = SF.Outer(SF.Inner().Expression);
+        StatementSyntax node = Factory.Outer(Factory.Inner().Expression);
 
         var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
 
@@ -70,22 +69,21 @@ public partial class UnitTest1
     public void Test3()
     {
 
-
-        [Template(typeof(SF), Options = TemplateOption.Single)]
+        [Template(typeof(Factory), Options = TemplateOption.Single)]
         static void Greeting(string message)
         {
             Console.WriteLine("Hello " + message);
         }
 
 
-        [Template(typeof(SF), Options = TemplateOption.Bare)]
+        [Template(typeof(Factory), Options = TemplateOption.Bare)]
         static void Repeater(Syntax item)
         {
             item();
             item();
         }
 
-        BlockSyntax node = SF.Repeater(SF.Greeting("World").Expression);
+        BlockSyntax node = Factory.Repeater(Factory.Greeting("World").Expression);
 
         var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
 
@@ -103,7 +101,7 @@ public partial class UnitTest1
     public void Test4()
     {
 
-        [Template(typeof(SF), Options = TemplateOption.Bare)]
+        [Template(typeof(Factory), Options = TemplateOption.Bare)]
         static void NoUnroll(int count)
         {
             int ret = 0;
@@ -113,7 +111,7 @@ public partial class UnitTest1
             }
         }
 
-        BlockSyntax node = SF.NoUnroll(4);
+        BlockSyntax node = Factory.NoUnroll(4);
 
 
         var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
@@ -129,36 +127,35 @@ public partial class UnitTest1
 
         Assert.Equal(expected, source);
     }
-
-
-    //[Fact]
-    //public void Test5()
-    //{
-    //    [Template(typeof(SF), Options = TemplateOption.Bare)]
-    //    static void Unroll([Unquote] int count)
-    //    {
-    //        int ret = 0;
-    //        for (int i = 0; i < count; i++)
-    //        {
-    //            ret++;
-    //        }
-    //    }
-
-    //    BlockSyntax node = SF.Unroll(4);
-
-
-    //    var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
-    //    string expected = """ 
-    //                          {
-    //                              int ret = 0;
-    //                                  ret++;
-    //                                  ret++;
-    //                                  ret++;
-    //                                  ret++; 
-    //                          }
-    //                          """;
-
-    //    Assert.Equal(expected, source);
-    //}
-
 }
+
+
+//[Fact]
+//public void Test5()
+//{
+//    [Template(typeof(Factory), Options = TemplateOption.Bare)]
+//    static void Unroll([Unquote] int count)
+//    {
+//        int ret = 0;
+//        for (int i = 0; i < count; i++)
+//        {
+//            ret++;
+//        }
+//    }
+
+//    BlockSyntax node = Factory.Unroll(4);
+
+
+//    var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
+//    string expected = """ 
+//                          {
+//                              int ret = 0;
+//                                  ret++;
+//                                  ret++;
+//                                  ret++;
+//                                  ret++; 
+//                          }
+//                          """;
+
+//    Assert.Equal(expected, source);
+//}
