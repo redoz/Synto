5ff6d9a91fa1f2e4d51a9f42d882438766e83ef0
Patrik Husfloen <redoz@redoz.com>
2022-12-18 16:36:59 +0100

    Add Sample project and attempt to push package

---
+++ samples/Synto.Samples/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+static void Simple()
+{
+    Console.WriteLine("Hello World");
+}
+
+partial class Factory { };
+
+//[Template(typeof(Factory), Options = TemplateOption.Single)]
+
+StatementSyntax node = Factory.Simple();
+
+var source = node.NormalizeWhitespace(eol: Environment.NewLine).GetText(Encoding.UTF8).ToString().Trim();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(source);
+
+
+
+
+
+
