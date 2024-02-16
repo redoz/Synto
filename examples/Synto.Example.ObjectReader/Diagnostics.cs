d75410fbc47c8180ca543fda10ede7ca467d28b1
Patrik Husfloen <redoz@redoz.com>
2024-02-16 23:39:28 +0100

    Work on ObjectReader example

---
+++ examples/Synto.Example.ObjectReader/Diagnostics.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using Synto.Diagnostics;
+
+namespace Synto.Example.ObjectReader;
+
+internal static partial class Diagnostics
+{
+    private const string IdPrefix = "SOR";
+
+    public static Diagnostic InternalError(Exception exception)
+    {
+        return InternalError(null, exception.GetType().FullName!, exception.ToString());
+    }
+
+    [Diagnostic(IdPrefix + "0000", "Internal Error", "Unhandled exception {0} was thrown: {1}", "Synto.Internal", DiagnosticSeverity.Error, true)]
+    public static partial Diagnostic InternalError(Location? location, string exceptionType, string exceptionMessage);
+}
---
+++ examples/Synto.Example.ObjectReader/ObjectReader.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace Synto.Example.ObjectReader;
+
+public static class ObjectReader
+{
+    public static IDataReader Create<T>(IEnumerable<T> data, params string[] properties)
+    {
+        throw new NotImplementedException();
+    }
+}
